10ac048e14160c9f6c3b6058692af5110b00f1a1
Sean-A1 <howcanisay49@gmail.com>
2025-02-04 19:14:15 +0900

    500 error

--- common/views.py
+++ common/views.py
@@ -26,3 +26,6 @@ def signup(request):
 
 def page_not_found(request, exception):
     return render(request, 'common/404.html', {})
+
+def Internal_Server_Error(request, exception):
+    return render(request, 'common/500.html', {})
