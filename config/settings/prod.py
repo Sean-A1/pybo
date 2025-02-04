8992f74544437ab230c33787077fa6c68b4933b7
Sean-A1 <howcanisay49@gmail.com>
2025-02-05 03:26:03 +0900

    domain

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['43.201.231.69']
+ALLOWED_HOSTS = ['43.201.231.69', 'tukcappybo.xyz']
 STATIC_ROOT = BASE_DIR / 'pybo/static/'
 STATICFILES_DIRS = []
 DEBUG = False
